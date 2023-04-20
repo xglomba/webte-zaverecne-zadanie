a0384e5c96efc9f4fd43bcdb562e3b9aa52b2e4f
marek_galik <galik.marekk@gmail.com>
2023-04-20 15:17:38 +0200

    Add auth

---
+++ app/zaverecne_zadanie/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+    path('home/', views.HomeView.as_view(), name='home'),
+    path('', views.LoginView.as_view()),
+]
--- app/zaverecne_zadanie/views.py
+++ app/zaverecne_zadanie/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.views import LogoutView
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views import View
 
-# Create your views here.
+from django.contrib.auth.decorators import user_passes_test
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def render_super_user_template(request, template):
+    return render(request, template)
+
+
+def render_student_template(request, template):
+    return render(request, template)
+
+
+class LoginView(View):
+    template_name = 'zaverecne_zadanie/login.html'
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            if request.user.is_superuser:
+                return render_super_user_template(request, 'zaverecne_zadanie/teacher/home.html')
+            else:
+                return render_student_template(request, 'zaverecne_zadanie/students/home.html')
+        return render(request, self.template_name)
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            if user.is_superuser:
+                return render_super_user_template(request, 'zaverecne_zadanie/teacher/home.html')
+            else:
+                return render_student_template(request, 'zaverecne_zadanie/students/home.html')
+        else:
+            messages.error(request, 'Username or password is not correct!')
+            return render(request, self.template_name, {'username': username})
+
+
+class CustomLogoutView(LogoutView):
+    template_name = 'zaverecne_zadanie/login.html'
+    next_page = reverse_lazy('login')
+
+
+class HomeView(View):
+    template_name = 'zaverecne_zadanie/home.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
